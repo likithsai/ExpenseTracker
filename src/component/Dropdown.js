84f7de438fb8e43baee9aa01fcadecc4e3e41aca
likithsai <likithsai13@gmail.com>
2022-01-05 19:40:35 +0530

    Added default text if no item is selected

--- src/component/Dropdown.js
+++ src/component/Dropdown.js
@@ -50,8 +50,8 @@ const Dropdown = (props) => {
                 style={[ props.style, { width: '100%', backgroundColor: '#fff', marginTop: 10, marginBottom: 20, flexDirection: 'row',  } ]}
                 onPress={() => setModalVisible(true)}
             >
-                <Icon name={selectedItem.itemIcon} color={props.itemColor} style={{ marginRight: 10 }} />
-                <Text style={{ color: props.itemColor, fontSize: 20 }}>{ selectedItem.itemName }</Text>
+                <Icon name={[ selectedItem.itemIcon || 'arrow-dropdown-circle' ]} color={props.itemColor} style={{ marginRight: 10 }} />
+                <Text style={{ color: props.itemColor, fontSize: 20 }}>{[ selectedItem.itemName || 'Please Select Invoice Type' ]}</Text>
             </TouchableOpacity>
         </>
     )
