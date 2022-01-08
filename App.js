9d6403e5e62d44921a7a9bbf9f31bdd4a23f4ea0
likithsai <likithsai13@gmail.com>
2022-01-08 20:08:26 +0530

    Create category database tables

--- App.js
+++ App.js
@@ -33,6 +33,27 @@ const ExpenseTracker = () => {
               }
           )
         })
+
+        //  category table
+        db.transaction(function(txn) {
+            txn.executeSql(
+              `CREATE TABLE IF NOT EXISTS tbl_category(
+                  category_id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                  category_name VARCHAR(20) DEFAULT NULL, 
+                  category_desc VARCHAR(500) DEFAULT NULL, 
+                  category_icon_type VARCHAR(20) DEFAULT NULL, 
+                  category_icon VARCHAR(20) DEFAULT NULL, 
+                  category_created timestamp default CURRENT_TIMESTAMP
+              )`,
+              [],
+              () => {
+                  console.log('table category created !')
+              }, 
+              (err) => {
+                  console.log('Error: ' + err.message)
+              }
+          )
+        })
     }, [])
     
     return (
