058ee22958dea1f5eaa930f929d1e51ea913f9e3
likithsai <likithsai13@gmail.com>
2022-01-24 21:20:02 +0530

    Added about and export opption in settings

--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -1,6 +1,9 @@
 import React from 'react'
-import { View, Text, Vibration } from 'react-native'
+import { View, Text, Vibration, ScrollView } from 'react-native'
 import HeaderCompStart from '../component/HeaderCompStart'
+import Card from '../component/Card'
+import Icon from 'react-native-ionicons'
+import FeatherIcons from 'react-native-vector-icons/Feather'
 
 const Settings = ({ navigation }) => {
     return (
@@ -20,6 +23,26 @@ const Settings = ({ navigation }) => {
                     })
                 }} 
             />
+            <ScrollView>
+                <Card style={{ padding: 20, borderBottomWidth: 0.5, borderBottomColor: '#ccc' }} onPress={() => {}}>
+                    <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-start' }}>
+                        <FeatherIcons name="download" size={30} color='#666' style={{ marginRight: 15 }} />
+                        <View>
+                            <Text style={{ color: '#000', fontWeight: 'bold', fontSize: 20 }}>Export</Text>
+                            <Text style={{ color: '#777' }}>Export Database</Text>
+                        </View>
+                    </View>
+                </Card>
+                <Card style={{ padding: 20 }} onPress={() => {}}>
+                    <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-start' }}>
+                        <FeatherIcons name="info" size={30} color='#666' style={{ marginRight: 15 }} />
+                        <View>
+                            <Text style={{ color: '#000', fontWeight: 'bold', fontSize: 20 }}>About</Text>
+                            <Text style={{ color: '#777' }}>Show the details about the app</Text>
+                        </View>
+                    </View>
+                </Card>
+            </ScrollView>
         </>
     )
 }
