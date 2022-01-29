be7f888bd09fb7b7a116747859ec892d05148fd6
likithsai <likithsai13@gmail.com>
2022-01-29 11:34:10 +0530

    Minor code changes (Changed the highlighted color for bar chart)

--- src/screens/DashboardScreeen.js
+++ src/screens/DashboardScreeen.js
@@ -25,16 +25,10 @@ const DashboardScreen = ({ navigation }) => {
                                 values: [{y: 100}, {y: 105}, {y: 102}, {y: 110}, {y: 114}, {y: 109}, {y: 105}, {y: 99}, {y: 95}, {y: 105}, {y: 99}, {y: 95}],
                                 label: '',
                                 config: {
-                                    color: [ 
-                                        processColor('#C0FF8C'), 
-                                        processColor('#FFF78C'), 
-                                        processColor('#FFD08C'), 
-                                        processColor('#11998ecc'), 
-                                        processColor('#FF8C9D')
-                                    ],
+                                    color: processColor('#11998ecc'), 
                                     barShadowColor: processColor('lightgrey'),
                                     highlightAlpha: 90,
-                                    highlightColor: processColor('#11998ecc'),
+                                    highlightColor: processColor('#ffff00'),
                                 }
                             }]
                         }}
