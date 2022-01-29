6fba62ac270745bfcb3241c34f83cb27cb9586ae
likithsai <likithsai13@gmail.com>
2022-01-29 11:04:54 +0530

    Added line and pie chart support

--- src/component/Header.js
+++ src/component/Header.js
@@ -4,8 +4,6 @@ import Icon from 'react-native-ionicons'
 import RBSheet from "react-native-raw-bottom-sheet"
 
 const Header = (props) => {
-    const refRBSheet = useRef()
-
     return (
         <>
         <View style={styles.boxBorder}>
@@ -40,6 +38,42 @@ const Header = (props) => {
                                 <Icon name="wallet" size={16} color='#555' style={{ marginRight: 10 }} />
                                 <Text style={{ fontWeight: 'bold', color: '#555', fontSize: 14 }}>Add Expenses</Text>
                             </View>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+        </View>
+        </>
+    )
+}
+
+const HeaderWithoutAddExpense = (props) => {
+    return (
+        <>
+        <View style={styles.boxBorder}>
+            <View>
+                <View style={[styles.boxBorder, { backgroundColor: '#11998e', paddingTop: 25, paddingBottom: 30, paddingHorizontal: 20, elevation: 5 }]}>
+                    <TouchableOpacity style={{ marginBottom: 20, flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }} onPress={() => { props.onDateSelected() }}>
+                        {/* <Text style={{ fontWeight: '900', color: '#ff0' }}>DATE : </Text> */}
+                        <Text style={{ fontWeight: 'bold', color: '#fff', fontSize: 16 }}>{props.dateText}</Text>
+                    </TouchableOpacity>
+                    <View style={{ width: '100%', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+                        <TouchableOpacity style={{ flexDirection: 'column', alignItems: 'center', borderRightColor: '#ccc' }}>
+                            <Icon name="card" size={40} color='#fff' style={{ paddingVertical: 5}} />
+                            <Text style={{ fontSize: 20, color: '#fff', fontWeight: 'bold' }}>Incomes</Text>
+                            <Text style={{ fontSize: 15, color: '#fff' }}>{props.incomeValue}</Text>
+                        </TouchableOpacity>
+                        <View style={{ borderColor: '#f0f0f0', borderWidth: 0.2, height: '100%' }} />
+                        <TouchableOpacity style={{ flexDirection: 'column', alignItems: 'center', borderRightColor: '#ccc'}}>
+                            <Icon name="cash" size={40} color='#fff' style={{ paddingVertical: 5}} />
+                            <Text style={{ fontSize: 20, color: '#fff', fontWeight: 'bold' }}>Expense</Text>
+                            <Text style={{ fontSize: 15, color: '#fff' }}>{props.expenseValue}</Text>
+                        </TouchableOpacity>
+                        <View style={{ borderColor: '#f0f0f0', borderWidth: 0.2, height: '100%' }} />
+                        <TouchableOpacity style={{ flexDirection: 'column', alignItems: 'center', borderRightColor: '#ccc' }}>
+                            <Icon name="wallet" size={40} color='#fff' style={{ paddingVertical: 5}} />
+                            <Text style={{ fontSize: 20, color: '#fff', fontWeight: 'bold' }}>Balance</Text>
+                            <Text style={{ fontSize: 15, color: '#fff' }}>{props.balanceValue}</Text>
                         </TouchableOpacity>
                     </View>
                 </View>
--- src/screens/DashboardScreeen.js
+++ src/screens/DashboardScreeen.js
@@ -1,6 +1,10 @@
 import React from 'react'
-import { View, Text, Vibration, ScrollView } from 'react-native'
+import { View, Text, Vibration, ScrollView, processColor } from 'react-native'
 import HeaderWithIcons from '../component/HeaderWithIcons'
+import {BarChart, PieChart} from 'react-native-charts-wrapper'
+import Card from '../component/Card'
+import Header from '../component/Header'
+import FeatherIcons from 'react-native-vector-icons/Feather'
 
 const DashboardScreen = ({ navigation }) => {
     return (
@@ -13,6 +17,110 @@ const DashboardScreen = ({ navigation }) => {
                 }}
             />
             <ScrollView>
+                <Header
+                    onDateSelected = {() => {
+                        Vibration.vibrate(50)
+                    }}
+                    incomeValue = {10}
+                    expenseValue = {20}
+                    balanceValue = {30}
+                    dateText = {'Month: DEC'}
+                    AddExpenseEnabled = {false}
+                />
+                <Card style={{ elevation: 5, borderBottomWidth: 0.7, borderBottomColor: '#ccc', height: 300, paddingVertical: 20, marginBottom: 1 }}>
+                    <Text style={{ fontSize: 20, fontWeight: 'bold', color: '#000' }}>Annual Expense</Text>
+                    <Text style={{ fontSize: 15, fontWeight: 'bold', color: '#999' }}>Representation of annual expenses in bar chart</Text>
+                    <BarChart
+                        style={{ width: '100%', height: '80%' }}
+                        data={{
+                            dataSets: [{
+                                values: [{y: 100}, {y: 105}, {y: 102}, {y: 110}, {y: 114}, {y: 109}, {y: 105}, {y: 99}, {y: 95}, {y: 105}, {y: 99}, {y: 95}],
+                                label: '',
+                                config: {
+                                    color: processColor('#11998e'),
+                                    barShadowColor: processColor('lightgrey'),
+                                    highlightAlpha: 90,
+                                    highlightColor: processColor('#11998ecc'),
+                                }
+                            }],
+                        }}
+                        yAxis={{
+                            left : {
+                                enabled: false
+                            },
+                            right : {
+                                enabled: false
+                            },
+                            textColor: '#fff'
+                        }}
+                        xAxis={{
+                            position: 'BOTTOM',
+                            drawAxisLines: false,
+                            drawGridLines: false,
+                            valueFormatter: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+                            granularityEnabled: true,
+                            granularity : 1,
+                            textColor: '#fff'
+                        }}
+                        chartDescription={{
+                            text: ''
+                        }}
+                        legend={{
+                            enabled: false,
+                        }}
+                        drawBarShadow={false}
+                        drawValueAboveBar={false}
+                        drawHighlightArrow={false}
+                        onSelect={(event) => {
+                            console.log(event.nativeEvent)
+                        }}
+                        onChange={(event) => console.log(event.nativeEvent)}
+                    />
+                </Card>
+                <Card style={{ elevation: 5, height: 300, paddingVertical: 20, marginBottom: 1 }}>
+                    <Text style={{ fontSize: 20, fontWeight: 'bold', color: '#000' }}>Expense By Category</Text>
+                    <Text style={{ fontSize: 15, fontWeight: 'bold', color: '#999' }}>Representation of overall category expense</Text>
+                    <PieChart
+                        style={{ width: '100%', height: '100%', padding: 20 }}
+                        data={{
+                            dataSets: [{
+                                values: [
+                                    {value: 45, label: 'Sandwiches'},
+                                    {value: 21, label: 'Salads'},
+                                    {value: 15, label: 'Soup'},
+                                    {value: 9, label: 'Beverages'},
+                                    {value: 15, label: 'Desserts'}
+                                ],
+                                config: {
+                                    colors: [
+                                        processColor('#C0FF8C'), 
+                                        processColor('#FFF78C'), 
+                                        processColor('#FFD08C'), 
+                                        processColor('#8CEAFF'), 
+                                        processColor('#FF8C9D')
+                                    ],
+                                    valueTextSize: 15,
+                                    sliceSpace: 5,
+                                    selectionShift: 13,
+                                    valueFormatter: "#.#'%'",
+                                    valueLinePart1Length: 0.5
+                                }
+                            }],
+                        }}
+                        chartDescription={{
+                            text: ''
+                        }}
+                        extraOffsets={{left: 20, top: 20, right: 20, bottom: 20}}
+                        entryLabelColor={processColor('#11998e')}
+                        entryLabelTextSize={20}
+                        drawEntryLabels={true}
+                        rotationEnabled={true}
+                        usePercentValues={true}
+                        holeRadius={0}
+                        maxAngle={360}
+                        onChange={(event) => console.log(event.nativeEvent)}
+                    />
+                </Card>
             </ScrollView>
         </>
     )
